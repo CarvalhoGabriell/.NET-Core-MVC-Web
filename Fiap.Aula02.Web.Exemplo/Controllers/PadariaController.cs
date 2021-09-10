6f0c039d788240b94d6c8515385cce0ba07b479b
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-09-09 22:46:26 -0300

    Finalizando CRUD de exemplos com dado mokados

--- Fiap.Aula02.Web.Exemplo/Controllers/PadariaController.cs
+++ Fiap.Aula02.Web.Exemplo/Controllers/PadariaController.cs
@@ -1,5 +1,6 @@
 ﻿using Fiap.Aula02.Web.Exemplo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,27 +11,70 @@ namespace Fiap.Aula02.Web.Exemplo.Controllers
     public class PadariaController : Controller
     {
         private static List<Padaria> _banco = new List<Padaria>();
-        
+        private static int _genador = 1;
+
+        [HttpGet]
+        // Carrega a view Index da Padaria, URL -> /Padaria ou /Padaria/index
         public IActionResult Index()
         {
             //Viewbag._banco
             return View(_banco);
         }
 
+        [HttpPost]
+        public IActionResult Remover(int id)
+        {
+            _banco.RemoveAll( num => num.Codigo == id);
+
+            // Remover pelo index procurado
+            //_banco.RemoveAt(_banco.FindIndex(num => num.Codigo == id));
+            TempData["msg"] = "Padaria Removida !!";
+
+            return RedirectToAction("index");
+        }
+
+        [HttpPost] // metodo responsavel para atualizar as informações quando apertar o botao "Editar"
+        public IActionResult Editar(Padaria padoca)
+        {
+            // atualizar com os dados novos informados
+            _banco[ _banco.FindIndex(num => num.Codigo == padoca.Codigo)] = padoca;
+            TempData["msg"] = "Padaria Editada com Sucesso!";
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]// abre a pagina de edição com os dados da padaria cadastrada
+        public IActionResult Editar(int id)
+        {
+            CarregarCidades();
+            // pesquisar uma padaria pelo codigo
+            var padaria = _banco.Find(num => num.Codigo == id);
+
+            // enviar para a view o objeto completo 
+            return View(padaria);
+        }
+
         [HttpGet]
         public IActionResult Cadastro()
         {
-
+            CarregarCidades();
             return View();
         }
 
-        [HttpPost]
+        [HttpPost]// enviar o formulario preenchido para cadastrar.
         public IActionResult Cadastro(Padaria padoca)
-        {   
+        {
+            padoca.Codigo = _genador++;
             _banco.Add(padoca);
-            TempData["msg"] = "Padaria Cadastrada com Sucesso";
+            TempData["msg"] = "Padaria Cadastrada com Sucesso!";
             return RedirectToAction("Cadastro");
    
+        }
+        // metodo statico que envia a lista  de cidades para carregar as opçoes do select
+        private void CarregarCidades()
+        {
+            var lista = new List<string>(new string[] { "São Paulo", "Rio de Janeiro", "Maceió", "Curitiba", "Fortaleza" });
+            ViewBag.cidades = new SelectList(lista);
         }
     }
 }
--- Fiap.Aula02.Web.Exemplo/Models/Padaria.cs
+++ Fiap.Aula02.Web.Exemplo/Models/Padaria.cs
@@ -1,22 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Fiap.Aula02.Web.Exemplo.Models
 {
     public class Padaria
     {
+        [HiddenInput] // define o campo para a view como um campo oculto
         public int Codigo { get; set; }
 
+        [Display(Name = "Disponivel")]
         public bool Disponivel { get; set; }
-
         public string Nome { get; set; }
 
-        [DataType(DataType.Date), Display(Name ="Data Abertura")]
+        [DataType(DataType.Date), Display(Name = "Data Abertura")]
         public DateTime DtAbertura { get; set; }
 
         public decimal Faturamento { get; set; }
+
+        public Tamanho Tamanhos { get; set; }
+
+        public string Cidade { get; set; }
     }
+    public enum Tamanho
+    {
+        Pequeno, Médio, Grande
+    }
+
 }
